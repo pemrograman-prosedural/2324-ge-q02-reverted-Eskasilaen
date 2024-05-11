9e3ec8dbd53ae3adb48576a36286ec020d8f61a4
Eskasilaen <eskasln01@gmail.com>
2024-05-11 17:12:16 +0700

    done

--- q02_01.c
+++ q02_01.c
@@ -12,32 +12,21 @@ int main(int _argc, char **_argv)
     char input[100];
     int dorm_count = 0;
     int dorm_size = 0;
-    struct dorm_t *drm = malloc(dorm_size * sizeof(struct dorm_t));
+    struct dorm_t *drm = NULL;
     int std_count = 0;
     int std_size = 0;
-    struct student_t *mhs = malloc(std_size * sizeof(struct student_t));
+    struct student_t *mhs = NULL;
     char *token;
 
     while (1)
     {
         fgets(input, sizeof(input), stdin);
-        while (1)
-        {
-            if (input[strlen(input)-1] == '\n' || input[strlen(input) - 1]== '\r')
-            {
-                input[strlen(input) - 1] = '\0';
-            }
-            else
-            {
-                break;
-            }
-        }
-        token = strtok(input, "#");
-        if(strcmp(token, "---")== 0)
-        {
+        if (strcmp(input, "---\n") == 0) {
             break;
         }
-        else if (strcmp(token, "dorm-add") == 0)
+        token = strtok(input, "#");
+        
+        if (strcmp(token, "dorm-add") == 0)
         {
             dorm_size++;
             drm = realloc(drm, dorm_size * sizeof(struct dorm_t));
@@ -97,6 +86,7 @@ int main(int _argc, char **_argv)
         else if (strcmp(token, "dorm-empty") == 0)
         {
             char *asrama = strtok(NULL, "#");
+        
             for (int i = 0; i < std_count; i++)
             {
                 if (mhs[i].dorm != NULL && strcmp(mhs[i].dorm->name, asrama) == 0)
@@ -105,11 +95,14 @@ int main(int _argc, char **_argv)
                 }
             }
         }
+
+        if (std_count > 0) {
+            printf("%s|%s|%s|%s|%s\n", mhs[std_count - 1].id, mhs[std_count - 1].name, mhs[std_count - 1].year, 
+                    mhs[std_count - 1].gender == GENDER_MALE ? "male" : "female",
+                    mhs[std_count - 1].dorm == NULL ? "unassigned" : mhs[std_count - 1].dorm->name);
+        }
     }
-    printf("%s|%s|%s|%s|%s\n", mhs[std_count - 1].id, mhs[std_count - 1].name, mhs[std_count - 1].year, 
-        mhs[std_count - 1].gender == GENDER_MALE ? "male" : "female",
-        mhs[std_count - 1].dorm == NULL ? "unassigned" : mhs[std_count - 1].dorm->name);
-        
+
     free(mhs);
     free(drm);
 
